801a05d1a83eeb814fd7e4c26d6a7786277bbd99
sidd-oo <sahoosiddharth3@gmail.com>
2022-07-31 15:13:38 +0530

    feat: pagination styling and logic

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
 import "./Pagination.css"
 
-const Pagination = ({currentPage, setCurrentPage}) => {
-  return (<div className="pagination">
-    <button disabled = {(currentPage === 1)} onClick={(e)=>{setCurrentPage(parseInt(e.target.innerText))}}>{currentPage - 1}</button>
+const Pagination = ({ currentPage, setCurrentPage }) => {
 
-    <button onClick={(e)=>{setCurrentPage(parseInt(e.target.innerText))}}>{currentPage}</button>
+  const handlePagination = (e) => {
+    setCurrentPage(parseInt(e.target.innerText))
+  }
 
-    <button disabled = {(currentPage === 10)}onClick={(e)=>{setCurrentPage(parseInt(e.target.innerText))}}>{currentPage + 1}</button>
-  </div>)
+  console.log(currentPage)
+
+  return (
+    <div className="pagination">
+      <a href="#">&laquo;</a>
+      <a href="#" className={currentPage === 1 ? "active" : ""} onClick={(e) => { handlePagination(e) }}>1</a>
+      <a href="#" className={currentPage === 2 ? "active" : ""} onClick={(e) => { handlePagination(e) }}>2</a>
+      <a href="#" className={currentPage === 3 ? "active" : ""} onClick={(e) => { handlePagination(e) }}>3</a>
+      <a href="#" className={currentPage === 4 ? "active" : ""} onClick={(e) => { handlePagination(e) }}>4</a>
+      <a href="#" className={currentPage === 5 ? "active" : ""} onClick={(e) => { handlePagination(e) }}>5</a>
+      <a href="#" className={currentPage === 6 ? "active" : ""} onClick={(e) => { handlePagination(e) }}>6</a>
+      <a href="#" className={currentPage === 7 ? "active" : ""} onClick={(e) => { handlePagination(e) }}>7</a>
+      <a href="#" className={currentPage === 8 ? "active" : ""} onClick={(e) => { handlePagination(e) }}>8</a>
+      <a href="#" className={currentPage === 9 ? "active" : ""} onClick={(e) => { handlePagination(e) }}>9</a>
+      <a href="#">&raquo;</a>
+    </div>)
 }
 
 export default Pagination
